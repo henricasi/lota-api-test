72b78f707f43a78eb5b20e478403c3cab5c1b71b
henricasi <henriquecmandruzato@gmail.com>
2023-03-23 12:33:25 -0300

    1o commit

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+import { fetchAPI } from '@/lib/api'
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({homeData, projectsData}) {
   return (
     <>
       <Head>
@@ -15,109 +17,102 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div>
+          <p>{homeData.attributes.Sobre}</p>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div>
+          {projectsData.map((project, index) => {
+            return <p>{project.attributes.title}</p>
+          })}
         </div>
       </main>
     </>
   )
+}
+
+export async function getStaticProps() {
+  // fetch homepage data
+  // const homeQuery = qs.stringify(
+  //   {
+  //     populate: "*",
+  //   },
+  //   {
+  //     encodeValuesOnly: true,
+  //   }
+  // );
+
+  const strapiHomeData = await fetchAPI(`home`);
+  const homeData = strapiHomeData.data;
+
+
+  // fetch projects data
+  // const projectsQuery = qs.stringify(
+  //   {
+  //     populate: "deep",
+  //   },
+  //   {
+  //     encodeValuesOnly: true,
+  //   }
+  // );
+
+  const strapiProjectsData = await fetchAPI(`projetos`);
+  let projectsData = strapiProjectsData.data;
+
+  // async function processCaptions(projectsArray) {
+  //   let projectsArrayCopy = [...projectsArray]
+  //   for (const project of projectsArrayCopy) {
+  //       let gallerySlides = project.attributes.gallery.slide
+  //       for (const slide of gallerySlides) {
+  //         if (slide.caption != null) {
+  //           let captionToHTML = await markdownToHTML(slide.caption)
+  //           slide.caption = captionToHTML
+  //         }
+  //       }
+  //     }
+  //   return projectsArrayCopy
+  // }
+
+  // let processedProjectsData = await processCaptions(projectsData)
+  // projectsData = processedProjectsData
+
+  // projectsData.forEach((project, index) => {
+    
+  //   project.attributes.gallery.slide.forEach((slide, index) => {
+  //     let newImagesArray = []
+  //     // iterar data (array de imagens dentro de slide)
+  //     slide.images.data.forEach((image, index) => {        
+  //       // para cada imagem, novo objeto apenas com campos desejados
+
+  //       let newImageObject = {
+  //         attributes: {
+  //           width: image.attributes.width,
+  //           height: image.attributes.height,
+  //           url: image.attributes.url,
+  //           formats: {}
+  //         }
+  //       }
+
+  //       if (image.attributes.formats && image.attributes.formats.large !== null && image.attributes.formats.large !== undefined) {
+  //         newImageObject.attributes.formats = {
+  //           large: {
+  //             width: image.attributes.formats.large.width,
+  //             height: image.attributes.formats.large.height,
+  //             url: image.attributes.formats.large.url,
+  //           }
+  //         }
+  //       }
+
+
+  //       newImagesArray.push(newImageObject)
+  //       slide.images.data = newImagesArray
+  //     })
+  //   })
+  // })
+
+  projectsData.sort((a, b) => new Date(b.attributes.date) - new Date(a.attributes.date))
+
+  return {
+    props: { homeData, projectsData },
+    revalidate: 10,
+  };
 }
